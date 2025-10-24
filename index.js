f6fb72f97d5ef215d75a9490c81b8291a647fb1c
Santi Burgos <151892199+SanB00@users.noreply.github.com>
2025-10-24 13:02:20 -0300

    Agregar_API

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import axios from "axios";
+
+
 console.log("Hola Mundo desde Node.js");
+const URL_PRODUCTS = "https://fakestoreapi.com/products";
+const ID_PRODUCT = "https://fakestoreapi.com/products/{id}".replace("{id}", 1);
+
+
+const response = await axios.get(URL_PRODUCTS);
+console.log(response.data);
